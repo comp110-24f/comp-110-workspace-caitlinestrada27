0c2d55ad3a9843fb1d8f086c29a0d0174c895d3a
abyrnes1 <alyssanbyrnes@gmail.com>
2024-05-12 22:21:10 -0400

    Updated welcome message

--- welcome.py
+++ welcome.py
@@ -1,7 +1,7 @@
 """A welcoming test program to start COMP110"""
 
-__author__ = "710453084"
+__author__ = "01234567"
 
-print("Welcome to the Spring 2024 of COMP110-003!")
+print("Welcome to the Summer Session 1 class of COMP110!")
 print("You are in for a fun adventure into the woods of programming!")
-print("<3 Kris & the COMP110 UTA Team")
+print("<3 Alyssa & the COMP110 UTA Team")
