6e130114826b39c53d7bd885b12ca4a26c6d3fe1
abyrnes1 <alyssanbyrnes@gmail.com>
2024-06-13 11:25:13 -0400

    Added ex06 files

---
+++ ex06/bear.py
@@ -0,0 +1,9 @@
+"""File to define Bear class."""
+
+class Bear:
+    
+    def __init__(self):
+        return None
+    
+    def one_day(self):
+        return None
---
+++ ex06/river.py
@@ -0,0 +1,63 @@
+"""File to define River class."""
+
+from ex06.fish import Fish
+from ex06.bear import Bear
+
+class River:
+    
+    day: int
+    fish: list[Fish]
+    bears: list[Bear]
+    
+    def __init__(self, num_fish: int, num_bears:int):
+        """New River with num_fish Fish and num_bears Bears"""
+        self.day: int = 0
+        self.fish: list[Fish] = []
+        self.bears: list[Bear] = []
+        # populate the river with fish and bears
+        for _ in range(0, num_fish):
+            self.fish.append(Fish())
+        for _ in range(0, num_bears):
+            self.bears.append(Bear())
+
+    def check_ages(self):
+        return None
+
+    def bears_eating(self):
+        return None
+    
+    def check_hunger(self):
+        return None
+        
+    def repopulate_fish(self):
+        return None
+    
+    def repopulate_bears(self):
+        return None
+    
+    def view_river(self):
+        return None
+            
+    def one_river_day(self):
+        """Simulate one day of life in the river"""
+        # Increase day by 1
+        self.day += 1
+        # Simulate one day for all Bears
+        for bear in self.bears:
+            bear.one_day()
+        # Simulate one day for all Fish
+        for fish in self.fish:
+            fish.one_day()
+        # Simulate Bear's eating
+        self.bears_eating()
+        # Remove hungry Bear's from River
+        self.check_hunger()
+        # Remove old Fish and Bear's from River
+        self.check_ages()
+        # Simulate Fish repopulation
+        self.repopulate_fish()
+        # Simulate Bear repopulation
+        self.repopulate_bears()
+        # Visualize River
+        self.view_river()
+            
