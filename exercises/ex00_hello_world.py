dcd34ae7f72dbdbc1877a692e5a99fc3d935ed6c
Caitlin Estrada <caitlin.estrada@unc.edu>
2024-08-27 10:35:45 -0400

    completed ex00 hello world

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,1 +1,10 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "630661469"
+
 print("Hello, World!")
+
+
+def greet(name: str) -> str:
+    """A welcoming first function defintion."""
+    return "Hello, " + name + "!"
