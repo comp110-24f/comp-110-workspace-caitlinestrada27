9ac3031d3054c814a9cf2ec8ccba930805b1eade
Caitlin Estrada <caitlin.estrada@unc.edu>
2024-11-14 11:24:08 -0500

    ex08

--- exercises/ex08/linked_list.py
+++ exercises/ex08/linked_list.py
@@ -1,6 +1,9 @@
 from __future__ import annotations 
 
+__author__ = "730661469"
+
 class Node: 
+    """Class to create Node of linked list."""
     value: int
     next: Node | None
 
@@ -14,19 +17,40 @@ class Node:
         else:
             rest: str = str(self.next)
             return f"{self.value} -> {rest}"
-        
-    def value_at(head: Node | None, index: int) -> int:
+
+
+def value_at(head: Node | None, index: int) -> int:
+    """Return value of Node stored at given index."""
+    if head is None:
         raise IndexError("Index is out of bounds on the list.")
-    
-    def max(head: Node | None) -> int:
+    if index == 0: 
+        return head.value 
+    return value_at(head.next, index - 1)
+
+
+def max(head: Node | None) -> int:
+    """Return int value of max value Node in linked list."""
+    if head is None: 
         raise ValueError("Cannot call max with None")
+    if head.next is None:
+        return head.value
+    next_max = max(head.next)
+    if head.value > next_max:
+        next_max = head.value
+    return next_max    
     
-    def linkify(items: list[int]) -> Node | None:
+
+def linkify(items: list[int]) -> Node | None:
+    """Slice list of integers and link together in linked list."""
+    if len(items) == 0:
         return None
+    return Node(items[0], linkify(items[1:]))
     
-    def scale(head: Node | None, factor: int) -> Node | None:
+
+def scale(head: Node | None, factor: int) -> Node | None:
+    """For every Node in linked list, multiply Node value by factor."""
+    if head is None:
         return None
-
-
-two: Node = Node(2, None)
-one: Node = Node(1, two)
+    if head.next is None:
+        return Node(head.value * factor, None)
+    return Node(head.value * factor, scale(head.next, factor))
